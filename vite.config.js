375d99a7f26876c1976811df74b62c4b60d46aa9
ADMIN <ADMIN@ANARA>
2026-04-23 18:57:50 +0530

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      // Your live Netlify/GoDaddy domain goes here
+      hostname: 'www.archaeotrails.com'
+    })
+  ],
 })
